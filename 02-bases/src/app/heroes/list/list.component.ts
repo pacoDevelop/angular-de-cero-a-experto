b211c80ea261561bc05c1a92b681b34dc80f1cd8
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-04-24 16:56:28 +0200

    49. Directiva *ngFor 50. Ng-template y el ngIf-else

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  public heroNames: string[] = ["Spiderman", "Iroman", "Hulk", "She Hulk", "Thor"];
+  public deletedHero?: string;
 
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+
+  }
 }
