12a8e63d620b7418d08c5e2f0960e12f8a328058
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-04-24 09:58:27 +0200

    24. Importaciones y exportaciones

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -7,7 +7,8 @@ import { setupCounter } from './counter.ts'
 // import './topics/03-functions.ts'
 // import './topics/04-homework-types.ts'
 // import "./topics/05-basic-destructuring.ts"
-import "./topics/06-function-destructuring.ts"
+// import "./topics/06-function-destructuring.ts"
+import "./topics/07-import-export.ts"
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola mundo`
 
