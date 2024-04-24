09fcf158d4c3f6d244030f73814d778a79a47b4c
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-04-24 12:05:44 +0200

    29. Genéricos

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -9,7 +9,8 @@ import { setupCounter } from './counter.ts'
 // import "./topics/05-basic-destructuring.ts"
 // import "./topics/06-function-destructuring.ts"
 // import "./topics/07-import-export.ts"
-import "./topics/08-classes.ts"
+// import "./topics/08-classes.ts"
+import "./topics/09-generics.ts"
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola mundo`
