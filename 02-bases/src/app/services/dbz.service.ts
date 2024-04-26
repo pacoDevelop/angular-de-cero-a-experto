d2f184d1e9b19b3ad8c37b9fa32ea6610af3f695
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-04-26 10:59:48 +0200

    72. Paquetes externos - UUID

--- 02-bases/src/app/services/dbz.service.ts
+++ 02-bases/src/app/services/dbz.service.ts
@@ -1,27 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../dbz/interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbzService {
   public characters: Character[] = [{
+    id: uuid(),
     name: "Krillin",
     power: 1000
   },
   {
+    id: uuid(),
     name: "Goku",
     power: 9500
   },
   {
+    id: uuid(),
     name: "Vegeta",
     power: 7500
   }]
   onNewCharacter(character: Character): void {
-    this.characters.push(character)
+    const newCharacter: Character = { ...character, id: uuid() }
+    this.characters.push(newCharacter)
   }
-  onDeleteCharacter(index: number): void {
-    this.characters.splice(index, 1)
+  onDeleteCharacter(id: string): void {
+    this.characters = this.characters.filter(character => character.id !== id)
   }
+
   constructor() { }
 }
