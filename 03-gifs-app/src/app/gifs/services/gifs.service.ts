7168b7ee733eebe9b685aabaeb4b79c8f82ec1c6
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-05-02 11:33:07 +0200

    96. Colocar un tipo de dato a una respuesta HTTP

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,15 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+
+  public gifList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
   private apiKey: string = "vngIvDRS2OILWpDqSqYH3QRYN2Ys9jEr"
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
@@ -36,9 +40,9 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag);
 
-    this.http.get(`${this.serviceUrl}/search`, { params })
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
       .subscribe(res => {
-        console.log(res)
+        this.gifList = res.data
       })
   }
 }
