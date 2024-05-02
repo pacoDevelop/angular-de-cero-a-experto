a621a04fd571d8b463eec1a6e5adfe4b2f1cd432
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-05-02 10:32:05 +0200

    93. Validaciones al servicio

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -4,16 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GifsService {
-  private _tagHistory: string[] = [];
+  private _tagsHistory: string[] = [];
 
   constructor() { }
 
   get tagsHistory() {
-    return [...this._tagHistory]
+    return [...this._tagsHistory]
+  }
+
+  public organizeHistory(tag: string) {
+    tag = tag.toLocaleLowerCase();
+    if (this.tagsHistory.includes(tag)) {
+      this._tagsHistory = this.tagsHistory.filter(oldTag => oldTag.toLocaleLowerCase() !== tag)
+    }
+    this._tagsHistory.unshift(tag)
+    this._tagsHistory = this.tagsHistory.splice(0, 10)
   }
 
 
   public searchTag(tag: string): void {
-    this._tagHistory.unshift(tag)
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
   }
 }
