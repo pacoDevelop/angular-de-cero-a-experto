abea4fe531e2d74267d22fe0a27a5f5ef9ff168c
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-05-06 14:00:36 +0200

    test

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 
-const BASE_URL = 'angular-de-cero-a-experto/04-country-app-deploy/';
-// const BASE_URL = '';
+// const BASE_URL = 'angular-de-cero-a-experto/04-country-app-deploy/';
+const BASE_URL = '';
 
 const routes: Routes = [
   {
@@ -17,7 +17,13 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: `home`
+    redirectTo: "/home",
+    pathMatch: "full"
+  },
+  {
+    path: '',
+    redirectTo: "/home",
+    pathMatch: "full"
   }
 ];
 
