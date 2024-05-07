5e995bda9f55a52a78c8ecc5538754129b07b0f8
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-05-07 12:18:17 +0200

    118. Resolución de la tarea

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 // const BASE_URL = 'angular-de-cero-a-experto/04-country-app-deploy/';
 const BASE_URL = '';
@@ -18,6 +19,14 @@ const routes: Routes = [
   {
     path: `about`,
     component: AboutPageComponent
+  },
+  {
+    path: `contact`,
+    component: ContactPageComponent
+  },
+  {
+    path: `countries`,
+    component: ContactPageComponent
   },
   {
     path: '**',
--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent],
+  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactPageComponent],
   imports: [
     CommonModule, RouterModule
   ],
