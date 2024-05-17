3e95300d48c1834c8c27f57203c116cff85fe995
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-05-17 22:48:57 +0200

    127. Tarea - País y Región

--- 04-countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-country-page',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class ByCountryPageComponent {
+  public countries: Country[] = [];
 
+  constructor(private serviceCountry: CountriesService) { }
+
+
+  searchByCountry(term: string) {
+
+    this.serviceCountry.searchCountry(term).subscribe(countries => this.countries = countries);
+
+  }
 }
