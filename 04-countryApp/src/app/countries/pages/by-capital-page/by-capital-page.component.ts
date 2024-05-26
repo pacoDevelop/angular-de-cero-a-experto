4695c6eb90b0f18135834bc4012589f866a746d1
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-05-26 22:42:51 +0200

    143. Mantener la data entre pantallas - Parte 2

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
 
@@ -7,12 +7,17 @@ import { Country } from '../../interfaces/country.interface';
   templateUrl: './by-capital-page.component.html',
   styles: []
 })
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnInit {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public initialValue: string = "";
 
   constructor(private serviceCountry: CountriesService) { }
+  ngOnInit(): void {
+    this.countries = this.serviceCountry.cacheStore.byCapital.countries;
+    this.initialValue = this.serviceCountry.cacheStore.byCapital.term;
+  }
 
 
   searchByCapital(term: string) {
