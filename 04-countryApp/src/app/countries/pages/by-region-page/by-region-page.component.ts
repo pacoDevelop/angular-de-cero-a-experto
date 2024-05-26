5e0e18a562e692c5437b75390be1e93d0bd33738
Francisco Hernandez Santos <franciscohernandezsantos3@gmail.com>
2024-05-26 21:41:03 +0200

    141. Buscar por region - Optimización

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,13 +12,15 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion: Region = 'Africa';
 
   constructor(private serviceCountry: CountriesService) { }
 
 
-  searchByRegion(region: string) {
+  searchByRegion(region: Region): void {
 
     this.serviceCountry.searchRegion(region).subscribe(countries => this.countries = countries);
-
+    this.selectedRegion = region;
   }
 }
